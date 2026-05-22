cabc91760e652968decbde989fbeef1b8d4508d8
windstart <boltjolt@qq.com>
2026-05-22 13:54:48 +0800

    chore(config): update announcement content to chinese
    Replace the original Japanese welcome announcement with Chinese version for better fit for Chinese users

--- src/config.ts
+++ src/config.ts
@@ -446,7 +446,7 @@ export const shareConfig: ShareConfig = {
 
 export const announcementConfig: AnnouncementConfig = {
 	title: "", // 公告标题，填空使用i18n字符串Key.announcement
-	content: "ブログへようこそ！これはサンプルの告知です", // 公告内容
+	content: "欢迎光临~~基于 Muzuki博客搭建，请多指教~~", // 公告内容
 	closable: true, // 允许用户关闭公告
 	link: {
 		enable: true, // 启用链接
